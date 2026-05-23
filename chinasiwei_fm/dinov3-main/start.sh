9c03f1367f3177c55979183ce601ac40d12e5b17
jingwang7236 <18511620029@163.com>
2026-05-23 15:59:20 +0800

    lora finetune based vit7b

--- chinasiwei_fm/dinov3-main/start.sh
+++ chinasiwei_fm/dinov3-main/start.sh
@@ -1,11 +1,27 @@
 # 单机单卡，从头预训练
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 TORCH_COMPILE_DISABLE=1 PYTHONPATH=${PWD} \
-python ./dinov3/train/train.py \
-    --config-file ./dinov3/configs/train/vit-large_chinasiwei/vitl_swdata_pretrain.yaml \
-    --output-dir work_dirs
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 TORCH_COMPILE_DISABLE=1 PYTHONPATH=${PWD} \
+# python ./dinov3/train/train.py \
+#     --config-file ./dinov3/configs/train/vit-large_chinasiwei/vitl_swdata_pretrain.yaml \
+#     --output-dir work_dirs
 
 # 单机多卡，从头预训练
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export CUDA_VISIBLE_DEVICES=0,1,2,3
+# export NCCL_DEBUG=WARN
+# export MASTER_PORT=39500
+# export PYTHONPATH=${PWD}
+# export CUDA_LAUNCH_BLOCKING=1 
+# export TORCH_COMPILE_DISABLE=1
+# # 使用 torchrun 启动分布式训练
+# torchrun \
+#     --nproc_per_node=4 \
+#     --master_port=$MASTER_PORT \
+#     ./dinov3/train/train.py \
+#     --config-file ./dinov3/configs/train/vit-large_chinasiwei/vitl_swdata_pretrain.yaml \
+#     --output-dir work_dirs/multi_gpu
+
+
+# 单机多卡，7b模型lora微调，预训练权重，bs=1,至少需要4卡
+export CUDA_VISIBLE_DEVICES=2,3,4,5
 export NCCL_DEBUG=WARN
 export MASTER_PORT=39500
 export PYTHONPATH=${PWD}
@@ -16,5 +32,5 @@ torchrun \
     --nproc_per_node=4 \
     --master_port=$MASTER_PORT \
     ./dinov3/train/train.py \
-    --config-file ./dinov3/configs/train/vit-large_chinasiwei/vitl_swdata_pretrain.yaml \
-    --output-dir work_dirs/multi_gpu
+    --config-file ./dinov3/configs/train/finetune/dinov3_vit7b16_lora_finetune.yaml \
+    --output-dir work_dirs/finetune/dinov3_vit7b16_lora_finetune
