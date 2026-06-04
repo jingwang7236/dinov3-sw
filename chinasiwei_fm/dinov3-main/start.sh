0327482464c2b5dc4215db4718293bd9e86a0f68
jingwang7236 <18511620029@163.com>
2026-06-04 15:08:15 +0800

    debug: eval iter lead to CUDA out of memory

--- chinasiwei_fm/dinov3-main/start.sh
+++ chinasiwei_fm/dinov3-main/start.sh
@@ -1,3 +1,4 @@
+# 50.9, conda: dinov3-wj2
 # 单机单卡，从头预训练
 # CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=1 TORCH_COMPILE_DISABLE=1 PYTHONPATH=${PWD} \
 # python ./dinov3/train/train.py \
@@ -26,7 +27,7 @@
 #     --output-dir work_dirs/vitl_gram_anchor
 
 # 单机多卡，7b模型lora微调，预训练权重，bs=1,至少需要4卡,,4卡训练时在eval阶段报错显存不足，导致没有保存pth权重
-export CUDA_VISIBLE_DEVICES=2,3,4,5
+export CUDA_VISIBLE_DEVICES=4,5,6,7
 export NCCL_DEBUG=WARN
 export MASTER_PORT=39600
 export PYTHONPATH=${PWD}
